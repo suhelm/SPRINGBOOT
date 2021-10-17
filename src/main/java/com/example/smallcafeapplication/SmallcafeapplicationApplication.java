50cafd3f86c9ce8142d927c496e3f9fac3545372
Mohammod Suhel Firdus <suhelfirdus@gmail.com>
2021-10-17 12:17:26 -0400

    Add files via upload

---
+++ src/main/java/com/example/smallcafeapplication/SmallcafeapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.example.smallcafeapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallcafeapplicationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmallcafeapplicationApplication.class, args);
+    }
+
+}
