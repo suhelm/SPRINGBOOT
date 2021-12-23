db2521b00b5f84e36438f65b088d926b3957c0a0
Mohammod Suhel Firdus <suhelfirdus@gmail.com>
2021-12-23 18:27:42 -0500

    Delete springtutorial directory

--- springtutorial/src/main/java/com/example/springtutorial/MultiplicationService.java
+++
@@ -1,8 +0,0 @@
-package com.example.springtutorial;
-
-public interface MultiplicationService {
-    /** crates a Multiplication object with two randomely
-     * generated factors between 11 and 99
-     */
-    Multiplication createRandomMultiplication();
-}
--- springtutorial/src/main/java/com/example/springtutorial/RandomGeneratorService.java
+++
@@ -1,5 +0,0 @@
-package com.example.springtutorial;
-
-public interface RandomGeneratorService {
-    int generateRandomFactor();
-}
